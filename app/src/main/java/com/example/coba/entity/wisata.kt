3724f519059d7874933705799cde5e7b61a5196a
theodorus96 <thedorusadiyatma96@gmail.com>
2022-09-10 16:55:40 +0700

    baru 3

---
+++ app/src/main/java/com/example/coba/entity/wisata.kt
@@ -0,0 +1,19 @@
+package com.example.coba.entity
+
+class wisata(var name: String, var lokasi: String) {
+    companion object{
+        @JvmField
+        var listOfWisata = arrayOf(
+            wisata("Kepulauan Derawan","Kalimantan Timur"),
+            wisata("Taman Nasional Way Kambas","Lampung"),
+            wisata("Pantai Parai Tenggiri","Bangka Belitung"),
+            wisata("Nusa Dua","Bali"),
+            wisata("Gunung Rinjani","Lombok, NTB"),
+            wisata("Danau Toba","Sumatera Utara"),
+            wisata("Nusa Penida","Bali"),
+            wisata("Taman Laut Bunaken","Sulawesi Utara"),
+            wisata("Wakatobi","Sulawesi Tenggara"),
+            wisata("Kepulauan Raja Ampat","Papua Barat")
+        )
+    }
+}
