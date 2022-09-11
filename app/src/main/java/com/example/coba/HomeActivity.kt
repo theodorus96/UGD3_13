297b68dbe864b46c5fb876eea3ee0600e2dda5e9
theodorus96 <thedorusadiyatma96@gmail.com>
2022-09-11 14:58:48 +0700

    terbaru nii

--- app/src/main/java/com/example/coba/HomeActivity.kt
+++ app/src/main/java/com/example/coba/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.coba
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,14 +9,38 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         loadFragment(FragmentWisata())
 
         changeFragment(FragmentWisata())
+        bottomNav= findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.profil -> {
+                    loadFragment(FragmentProfil())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.wisata -> {
+                    loadFragment(FragmentWisata())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.menu_exit -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    val mBundle = Bundle()
+                    mBundle.putString("username","")
+                    mBundle.putString("password","")
+                    intent.putExtra("register", mBundle)
+
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     fun changeFragment(fragment: Fragment?){
