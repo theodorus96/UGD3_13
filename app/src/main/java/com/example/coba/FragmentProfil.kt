5e029f6a810013f2aac57cb325df0618045e2671
Neydeline07 <dneydeline@gmail.com>
2022-09-11 15:58:09 +0700

    icon

--- app/src/main/java/com/example/coba/FragmentProfil.kt
+++ app/src/main/java/com/example/coba/FragmentProfil.kt
@@ -8,15 +8,6 @@ import android.view.ViewGroup
 
 
 class FragmentProfil : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +16,7 @@ class FragmentProfil : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil2, container, false)
     }
+
 
     companion object {
         @JvmStatic
